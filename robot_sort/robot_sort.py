26fa56d4e919417a57fdd4d3a99d8ea408b21bd4
Roberto Banbanaste <rbanbanaste@outlook.com>
2020-05-22 15:01:42 -0400

    almost

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -81,11 +81,13 @@ class SortingRobot:
         Turn on the robot's light
         """
         self._light = "ON"
+
     def set_light_off(self):
         """
         Turn off the robot's light
         """
         self._light = "OFF"
+
     def light_is_on(self):
         """
         Returns True if the robot's light is on and False otherwise.
@@ -96,15 +98,84 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        # sort the array through built in methods
+        # at each place in the array check current "held" value against value at position
+        # can only move in one dirrection until the end of the list
+
+        # sort ascending when moving from left to right
+        # -> check if held is greater than item at position
+        # -> move right if it is
+        # -> swap if it isnt
+        # -> continue until end of list
+
+        # sort descending when moving right to left
+        # -> check if held is less than item at position
+        # -> move left if it is
+        # -> swap if it isnt
+        # -> continue until end of list
+
+        """
+        Compare the held item with the item in front of the robot:
+        If the held item's value is greater, return 1.
+        If the held item's value is less, return -1.
+        If the held item's value is equal, return 0.
+        If either item is None, return None.
+        """
+
+        """ 
+        set light on
+        continue sorting while light is on
+        before each round set light off
+        if item was sorted set light on during round
+        continue untill light is not turned on 
+
+        while can move right 
+        swap if held item is None
+        check item against position item
+        if None move right
+        if item > position item move right
+        if item < position item swap items and move right
+        stop when you reach end of list
+
+        while can move left
+        swap if held item is None
+        check item against position item
+        if None move left 
+        if item < position item move left
+        if item > position item swap items and move left
+        stop when you reach begining of list
+        """
+
+        self.set_light_on()
+
+        while self.light_is_on():
+
+            self.set_light_off()
+
+            while self.can_move_right():
+                if self.compare_item() is None:
+                    self.swap_item()
+                    self.set_light_off()
+                elif self.compare_item() == -1:
+                    self.set_light_on()
+                    self.swap_item()
+                elif self.compare_item() == 1:
+                    self.set_light_off()
+                self.move_right()
+            else:
+                self.move_right()
+
+            while self.can_move_left():
+                self.move_left()
+            else:
+                self.move_left()
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [4, 27, 1, 18, 28, 17, 20, 2, 44, 50]
 
     robot = SortingRobot(l)
 
